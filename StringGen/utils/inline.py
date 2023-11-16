d66eeceacf762060ceb931dfe3e9fe9e934669af
ʀσƈƙყ <146221888+ROCKY-D3V@users.noreply.github.com>
2023-11-16 19:08:08 +0530

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -9,7 +9,7 @@ keyboard = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="sᴏᴜʀᴄᴇ", url="https://github.com/ROCKS143014/XDSTRINGGEN"
+                text="sᴏᴜʀᴄᴇ", url="https://t.me/N91Ab/6"
             ),
         ],
     ]
